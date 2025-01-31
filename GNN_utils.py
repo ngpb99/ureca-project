03f3fa1268230bd3669cd1ccb9677283e239b5ad
ngpb99 <ngpb1999@gmail.com>
2025-01-31 17:25:36 +0800

    Update GNN_utils.py

--- GNN_utils.py
+++ GNN_utils.py
@@ -244,3 +244,85 @@ class LipoFeatures(Dataset): # Dataset class is a general class that contains a 
             data = torch.load(os.path.join(self.processed_dir, 
                                  f'data_{idx}.pt'))        
         return data
+
+
+class LogPFeatures(Dataset): #Dataset class is a general class that contains a few functions to help convert mols to features.
+    def __init__(self, root, filename, test=False, transform=None, pre_transform=None):
+        """
+        root = Where the dataset should be stored. This folder is split
+        into raw_dir (downloaded dataset) and processed_dir (processed data). 
+        """
+        self.test = test
+        self.filename = filename
+        super(LogPFeatures, self).__init__(root, transform, pre_transform)
+        #calls the parent class (Dataset) allocate variables root, transform, and pre-transform.
+        #parent class calls some of the functions listed below from its __init__ function.
+        
+    @property
+    def raw_file_names(self):
+        """ If this file exists in raw_dir, the download is not triggered.
+            (The download func. is not implemented here)  
+        """
+        #what this means is that if we do not have the raw dataset available, we can call the download function to download the appropriate dataset.
+        #however, since we have the data in the raw folder, we will not use this function.
+        #this part is not coded as a result.
+        return self.filename
+    
+    @property
+    def processed_file_names(self):
+        self.data = pd.read_csv(self.raw_paths[0]).reset_index()
+        return [f"data_{i}.pt" for i in list(self.data.index)]
+        
+    def download(self):
+        pass
+    #pass as download of raw data is not required since we have it. Can code it if we want to download file from somewhere.
+    
+    #key function here to process the data into features, called by parent class.
+    def process(self):
+        self.data = pd.read_csv(self.raw_paths[0]).reset_index()
+        featurizer = dc.feat.MolGraphConvFeaturizer(use_edges=True)
+        #this variable is used to convert molecules to its respective features. Can hover mouse over and read description.
+        
+        for index, row in tqdm(self.data.iterrows(), total=self.data.shape[0]):
+            # Featurize molecule
+            mol = Chem.MolFromSmiles(row["smiles"]) #Change the col name repsectively
+            #convert smiles to mol
+            
+            f = featurizer._featurize(mol)
+            #convert mol to features
+            
+            data = f.to_pyg_graph()
+            #python object modeling a single graph with various attributes (features)
+            #gives a graph representation for the molecule and includes their respective features (edge and node) on the graph.
+            
+            data.y = self._get_label(row["LogP"]) #Change the col name repsectively
+            #adds a label to the graphical representation, telling us the expected value of the graph.
+            
+            data.smiles = row["smiles"] #Change the col name repsectively
+            if self.test:
+                torch.save(data, 
+                    os.path.join(self.processed_dir, 
+                                 f'data_test_{index}.pt'))
+            else:
+                torch.save(data, 
+                    os.path.join(self.processed_dir, 
+                                 f'data_{index}.pt'))
+                
+    def _get_label(self, label):
+        label = np.asarray([label])
+        return torch.tensor(label, dtype=torch.float32)
+
+    def len(self):
+        return self.data.shape[0]
+
+    def get(self, idx):
+        """ - Equivalent to __getitem__ in pytorch
+            - Is not needed for PyG's InMemoryDataset
+        """
+        if self.test:
+            data = torch.load(os.path.join(self.processed_dir, 
+                                  f'data_test_{idx}.pt'))
+        else:
+            data = torch.load(os.path.join(self.processed_dir, 
+                                  f'data_{idx}.pt'))        
+        return data
